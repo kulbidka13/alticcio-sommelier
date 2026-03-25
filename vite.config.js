6d762c595acafc2faac92b6afe837c46a210fda0
kulbidka13 <kulbidka@gmail.com>
2026-03-25 08:26:17 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,5 @@
-import { defineConfig } from "vite"; import react from "@vitejs/plugin-react"; export default defineConfig({ plugins: [react()], root: process.cwd() });
+import { defineConfig } from "vite"; 
+import react from "@vitejs/plugin-react"; 
+export default defineConfig({ 
+  plugins: [react()], 
+});
